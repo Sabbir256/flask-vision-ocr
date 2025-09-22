7a41b84a377b35ce3b75e147991920a13af86634
Md. Sabbir Ahmed <29686831+Sabbir256@users.noreply.github.com>
2025-09-22 20:56:25 +0600

    feat: read file and validate it

--- app/routes/ocr_routes.py
+++ app/routes/ocr_routes.py
@@ -1,7 +1,26 @@
 from flask import Blueprint, request, jsonify
+from app.utils.validators import validate_file
+from werkzeug.exceptions import RequestEntityTooLarge
 
 ocr_bp = Blueprint('ocr', __name__)
 
 @ocr_bp.route('/extract-text', methods=['POST'])
 def extract_text():
-    return jsonify({"message": "this is extracted text"}), 200
+    try:
+        if 'file' not in request.files:
+            return jsonify({"success": False, "message": "No file provided"}), 400
+        
+        file = request.files['file']
+        
+        if not validate_file(file):
+            return jsonify({"success": False, "message": "Invalid file type"}), 400
+        
+        return jsonify({"success": True, "text": "Extracted text"}), 200
+    
+    except RequestEntityTooLarge:
+        return jsonify({
+            "success": False, 
+            "message": "Uploaded file is too large. Maximum file size is 10MB."
+        }), 413
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e)}), 500
---
+++ app/utils/validators.py
@@ -0,0 +1,17 @@
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename \
+        and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def validate_file(file):
+    if not file or file.filename == '':
+        return False
+    
+    filename = secure_filename(file.filename)
+    if not allowed_file(filename):
+        return False
+    
+    return True
