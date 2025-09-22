981d1c7800ee957f23dec1acf65c924890e28fa8
Md. Sabbir Ahmed <29686831+Sabbir256@users.noreply.github.com>
2025-09-22 19:24:36 +0600

    feat: add dotenv package to load env files

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
+    MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # 10MB max file size
