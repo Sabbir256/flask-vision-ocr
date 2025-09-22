08b58e9ae286f58cc910959dc991edb47a8f9c9c
Md. Sabbir Ahmed <29686831+Sabbir256@users.noreply.github.com>
2025-09-23 00:39:10 +0600

    feat: docker setup and configurations

--- config.py
+++ config.py
@@ -6,4 +6,3 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
     MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # 10MB max file size
-    GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
